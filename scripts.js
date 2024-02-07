6f93993f9887fd43ddc077ef43b3525d54d12c95
unknown <peter_gilkey@yahoo.com>
2024-02-07 17:37:48 -0600

    file onchange handler

--- scripts.js
+++ scripts.js
@@ -2,9 +2,17 @@
 
 // const pdf = require('./files/files/ORD_138_22434164_20240206_2024020619332883.pdf');
 
-let orderFile = document.getElementById("orderFile");
+// let orderFile = document.getElementById("orderFile");
 let invoiceFile = document.getElementById("invoiceFile");
 const submitButton = document.getElementById("submitButton");
+
+
+const fileInput = document.getElementById('orderFile');
+fileInput.onchange = () => {
+  const selectedFile = fileInput.files[0];
+  console.log(selectedFile);
+}
+
 
 submitButton.addEventListener('click', () => {
     console.log(orderFile);
