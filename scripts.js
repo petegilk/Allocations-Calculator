01350454bfa30ae47ab3e47c87da5b56e83cf8d0
petegilk <peter_gilkey@yahoo.com>
2024-02-08 04:17:43 -0600

    added input onchange events

--- scripts.js
+++ scripts.js
@@ -1,22 +1,30 @@
 'use strict'
 
-// const pdf = require('./files/files/ORD_138_22434164_20240206_2024020619332883.pdf');
 
-// let orderFile = document.getElementById("orderFile");
-let invoiceFile = document.getElementById("invoiceFile");
+// Node.js declarations
+
+// const fs = require('fs');
+// const PDFParser = require('pdf2json');
+
+
+const orderInput = document.getElementById('orderInput');
+const invoiceInput = document.getElementById('invoiceInput');
 const submitButton = document.getElementById("submitButton");
 
+let orderPDF;
+let invoicePDF;
 
-const fileInput = document.getElementById('orderFile');
-fileInput.onchange = () => {
-  const selectedFile = fileInput.files[0];
-  console.log(selectedFile);
+orderInput.onchange = () => {
+  orderPDF = orderInput.files[0];
+  console.log(orderPDF);
+}
+
+invoiceInput.onchange = () => {
+  invoicePDF = invoiceInput.files[0];
+  console.log(invoicePDF);
 }
 
 
-submitButton.addEventListener('click', () => {
-    console.log(orderFile);
-})
+// submitButton.addEventListener('click', () => {
 
-console.log(orderFile);
-console.log(invoiceFile);
+// })
